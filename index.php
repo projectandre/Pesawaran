52ed6b1ea91ad2df41bff92731e2cfd8e0ee5ba9
andreagung1111 <andreagung1111@gmail.com>
2023-05-10 11:30:56 +0700

    PSWR

---
+++ index.php
@@ -0,0 +1,183 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Data Penduduk Pesawaran</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+    <link rel="shorcut icon" href="logo.png">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://cdn.jsdelivr.net/npm/html5shiv@3.7.3/dist/html5shiv.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/respond.js@1.4.2/dest/respond.min.js"></script>
+    <![endif]-->
+
+    <style>
+        html {
+            scroll-behavior: smooth;
+        }
+
+        body {
+            background-color: #FBFBFB;
+
+        }
+
+
+
+        .hrJudul {
+            width: 100px;
+            border-top: 3px solid black;
+        }
+    </style>
+</head>
+
+<body>
+    <!-- Home -->
+    <!--  <br><br><br><br><br><br> -->
+    <section class="home" id="home" style="margin-top: 100px;">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-sm-6 text-center">
+                    <img src="logo.png" width="200" alt="none">
+                </div>
+                <div class="col-sm-6 "> <!-- kasih offset biar bisa ngasih ruang kosong sebanyak 1 1 -->
+                    <p align="justify" style="font-size: 20px; font-family: sans-serif;"><strong>Provinsi Lampung</strong></p>
+                    <h1><b>Data Penduduk & Pendidikan Pesawaran</b></h1><br>
+                    <p align="justify" style="font-size: 15px; font-family: sans-serif;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo..ss</p><br>
+                    <a href="#Web" class="btn" style="background-color: #22CAFF; color:white; border-radius: 10px; height: 40px; line-height: 25px;">Selengkapnya !</a>
+                </div>
+
+            </div>
+        </div>
+    </section><br><br>
+    <!-- akhir Home -->
+
+    <!-- Jasa Web yang ditawarkan -->
+    <section class="Web" id="Web" style="min-height: 370px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 text-center">
+                    <h2><b>Jumlah Penduduk Pria dan Wanita</b></h2>
+                    <hr class="hrJudul">
+                </div>
+            </div>
+            <br><br>
+            <div class="row">
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail" style="box-shadow: 0px 0px 5px black; padding: 15px;">
+                        <div class="caption">
+                            <p style="font-size: 20px;"><b>Kec. GEDONG TATAAN</b></p>
+                            <p style="font-size: 13px;">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                            <a href="pddk_PriaWanita_gdtataan.php" class="btn" style="background-color: #22CAFF; color:white; border-radius: 10px; height: 40px; line-height: 25px;" role="button">
+                                <p>Lihat <img src="panahKanan.png" alt="Panah" width="15"></p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail" style="box-shadow: 0px 0px 5px black; padding: 15px;">
+                        <div class="caption">
+                            <p style="font-size: 20px;"><b>Kec. NEGERI KATON</b></p>
+                            <p style="font-size: 13px;">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                            <a href="pddk_PriaWanita_ngrikraton.php" class="btn" style="background-color: #22CAFF; color:white; border-radius: 10px; height: 40px; line-height: 25px;" role="button">
+                                <p>Lihat <img src="panahKanan.png" alt="Panah" width="15"></p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail" style="box-shadow: 0px 0px 5px black; padding: 15px;">
+                        <div class="caption">
+                            <p style="font-size: 20px;"><b>Kec. TEGINENENG</b></p>
+                            <p style="font-size: 13px;">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                            <a href="pddk_PriaWanita_tegineneng.php" class="btn" style="background-color: #22CAFF; color:white; border-radius: 10px; height: 40px; line-height: 25px;" role="button">
+                                <p>Lihat <img src="panahKanan.png" alt="Panah" width="15"></p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+        </div>
+    </section>
+
+    <!-- Jasa Web yang ditawarkan -->
+    <section class="Web" id="Web" style="min-height: 320px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 text-center">
+                    <h2><b>Jumlah Pendidikan Pria dan Wanita</b></h2>
+                    <hr class="hrJudul">
+                </div>
+            </div>
+            <br><br>
+            <div class="row">
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail" style="box-shadow: 0px 0px 5px black; padding: 15px;">
+                        <div class="caption">
+                            <p style="font-size: 20px;"><b>Kec. GEDONG TATAAN</b></p>
+                            <p style="font-size: 13px;">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                            <a href="pendidik_PriaWanita_gdtataan.php" class="btn" style="background-color: #22CAFF; color:white; border-radius: 10px; height: 40px; line-height: 25px;" role="button">
+                                <p>Lihat <img src="panahKanan.png" alt="Panah" width="15"></p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail" style="box-shadow: 0px 0px 5px black; padding: 15px;">
+                        <div class="caption">
+                            <p style="font-size: 20px;"><b>Kec. NEGERI KATON</b></p>
+                            <p style="font-size: 13px;">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                            <a href="pendidik_PriaWanita_ngrikraton.php" class="btn" style="background-color: #22CAFF; color:white; border-radius: 10px; height: 40px; line-height: 25px;" role="button">
+                                <p>Lihat <img src="panahKanan.png" alt="Panah" width="15"></p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail" style="box-shadow: 0px 0px 5px black; padding: 15px;">
+                        <div class="caption">
+                            <p style="font-size: 20px;"><b>Kec. TEGINENENG</b></p>
+                            <p style="font-size: 13px;">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                            <a href="pendidik_PriaWanita_tegineneng.php" class="btn" style="background-color: #22CAFF; color:white; border-radius: 10px; height: 40px; line-height: 25px;" role="button">
+                                <p>Lihat <img src="panahKanan.png" alt="Panah" width="15"></p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+        </div>
+    </section>
+
+
+
+    <!-- Footer -->
+    <!--  <br><br><br><br><br><br> -->
+    <section class="footer" id="footer" style="min-height: 50px;">
+        <div class="container"><br>
+
+            <div class="row" align="center">
+                @copyright by Andre Agung
+            </div>
+
+    </section>
+    <!-- akhir Footer -->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ pendidik_PriaWanita_gdtataan.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gedung Tataan Penduduk Pria dan Wanita</title>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+    <style>
+        html {
+            scroll-behavior: smooth;
+        }
+
+        body {
+            background-color: #FBFBFB;
+
+        }
+
+
+
+        .hrJudul {
+            width: 100px;
+            border-top: 3px solid black;
+        }
+    </style>
+
+</head>
+
+<body>
+    <div class="row">
+        <div class="col-sm-12 text-center">
+            <h2><b>Jumlah Pendidikan Pria dan Wanita Kec Gedung Tataan</b></h2>
+            <hr class="hrJudul">
+        </div>
+    </div>
+
+    <div class="thumbnail">
+        <div class="col-sm-6">
+            <canvas id="myChartL"></canvas>
+        </div>
+
+        <div class="col-sm-6">
+            <canvas id="myChartP"></canvas>
+        </div>
+    </div>
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
+    <script>
+        // URL endpoint
+        <?php
+
+        $headers = [
+            "Talent-token: PJ4HFKOK9BRMFEECJL1YJ3R7DX9V92J7XI4BY9IL",
+            "Talent-secret: etM3OpDZZyUs43z03RNLGtaCm4bJn5sWWRMwB3Lqn8Z41AlU7qSY6mfqSottH6AI6BBh0Q8idnotTOziXxpUJ0NY1KpiRoowvfZrO0DQzbpgZOsXC70IGxZj3PB46b6pIb4BDm804FYKw8A5pSBljhJhwsErZ92zIgRdFFFh9xyo7tlwx4LUW7JSkRFe4te26KCIjjK2TjGkxGVcbm96dJHkWsgfwDRo1bjG46osHKbPOzI69xDZKiow86Zw8Pn3kQyiRE50TEttvJgIMV3lDRNfCyZXzY5UgDSnd98d4jiV",
+        ];
+
+        // buat request
+        // API yang digunakan adalah NewsAPI : https://newsapi.org/
+
+        $ch = curl_init();
+
+        // curl_se($toptch, CURLOPT_URL, "https://newsapi.org/v2/top-headlines?category=general&country=id&apiKey=8642ac8d640f4a3c98046ce6e84b294e");
+        curl_setopt($ch, CURLOPT_URL, 'https://talent.pesawarankab.go.id/api_v01/akses/jumlah/pendidikan');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+
+
+        $data = curl_exec($ch);
+
+        // ubah response
+        $data = json_decode($data, true);
+        $data = $data['data'];
+        curl_close($ch);
+
+        // var_dump($data);
+
+        ?>
+
+        const labels = [ // ['januari','februari']
+            <?php
+            $a = 0;
+            foreach ($data as $dt) {
+                if ($dt['nama_kecamatan'] === "Gedong Tataan") {
+                    $a++;
+                    echo " ' " .  $dt['jenis_pendidikan'] . " ' ";
+                    echo ',';
+
+                    if ($a == 10) {
+                        break;
+                    }
+                }
+            }
+            ?>
+
+        ];
+
+        const data = {
+            labels: labels,
+            datasets: [{
+                label: 'Jumlah Pendidikan Pria di Kecamatan Gedong Tataan',
+                backgroundColor: 'skyblue',
+                borderColor: 'orange',
+                data: [
+                    <?php
+                    $b = 0;
+                    foreach ($data as $dt) {
+                        if ($dt['nama_kecamatan'] === "Gedong Tataan") {
+                            echo $dt['laki'];
+                            echo ',';
+
+                            if ($b == 10) {
+                                break;
+                            }
+                        }
+                        $b++;
+                    }
+                    ?>
+                ],
+            }]
+        };
+
+        const config = {
+            type: 'bar', // doughnut, bar, line
+            data: data,
+            options: {
+                plugins: {
+                    legend: {
+                        display: false
+                    },
+                    title: {
+                        display: true,
+                        text: "Data Pendidikan Pria"
+                    }
+                }
+            }
+        };
+
+        var myChart = new Chart(
+            document.getElementById('myChartL'),
+            config
+        );
+
+
+
+
+        const labelsP = [ // ['januari','februari']
+            <?php
+            $a = 0;
+            foreach ($data as $dt) {
+                if ($dt['nama_kecamatan'] === "Gedong Tataan") {
+
+                    echo " ' " .  $dt['jenis_pendidikan'] . " ' ";
+                    echo ',';
+
+                    if ($a == 10) {
+                        break;
+                    }
+                    $a++;
+                }
+            }
+            ?>
+
+        ];
+
+        const dataP = {
+            labels: labelsP,
+            datasets: [{
+                label: 'Jumlah Pendidikan Wanita di Kecamatan Gedung Tataan',
+                backgroundColor: 'Orange',
+                borderColor: 'orange',
+                data: [
+                    <?php
+                    $b = 0;
+                    foreach ($data as $dt) {
+
+                        if ($dt['nama_kecamatan'] === "Gedong Tataan") {
+                            $b++;
+                            echo $dt['perempuan'];
+                            echo ',';
+
+                            if ($b == 10) {
+                                break;
+                            }
+                        }
+                    }
+                    ?>
+                ],
+            }]
+        };
+
+        const configP = {
+            type: 'bar', // doughnut, bar, line
+            data: dataP,
+            options: {
+                plugins: {
+                    legend: {
+                        display: false
+                    },
+                    title: {
+                        display: true,
+                        text: "Data Pendidikan Wanita"
+                    }
+                }
+            }
+        };
+
+        var myChartP = new Chart(
+            document.getElementById('myChartP'),
+            configP
+        );
+    </script>
+</body>
+
+</html>
